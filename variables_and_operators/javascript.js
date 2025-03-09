19b54c6289ecfd4a9c96d735698470c81413fbc1
Maria <100872877+Maria00002@users.noreply.github.com>
2025-03-09 15:32:33 -0700

    Variables and Operators
    Running JacaScript code using an HTML file.
    Declaring variables with let and const.
    Performing number opations, string operations.
    Use logical and mathematical operators.

---
+++ variables_and_operators/javascript.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+// pi = 10;
+
+console.log(pi);
+
+//Two ways to declare a variable:
+// let, which we can re-assign
+// const which we can't re-assign and will throw an error if we try
+
+console.log(3+2-76*(1+1));
