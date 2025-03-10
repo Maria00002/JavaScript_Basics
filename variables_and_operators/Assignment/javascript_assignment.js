f26fab90430daead93e889c55cfd95bb4e36f9f4
Maria <100872877+Maria00002@users.noreply.github.com>
2025-03-09 18:39:15 -0700

    Javascript assignment
    Finished the assigned questions on javascript
    variables.

---
+++ variables_and_operators/Assignment/javascript_assignment.js
@@ -0,0 +1,19 @@
+console.log(23+97);
+
+console.log(1+2+3+4+5+6); //7*3=21
+
+console.log((4+6+9)/77);
+
+let a = 10;
+console.log(a);
+
+a = 6;
+console.log(a);
+
+let b = 7*a; //7*6=42
+console.log(b); //42
+
+const max = 57;
+const actual = max -13; //57-13=44
+const percentage = actual/max;
+console.log(percentage); //0.7719
