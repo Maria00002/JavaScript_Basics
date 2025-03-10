f56bf301609cea86543800f0dee1ca5151d57af3
Maria <100872877+Maria00002@users.noreply.github.com>
2025-03-10 12:45:42 -0700

    Exercised covered in the variables in javascript from JavaScript.Info
    Learned and practiced variables in javaScript

---
+++ variables_and_operators/Practices/javascript_practices.js
@@ -0,0 +1,17 @@
+// Working with variables
+let admin;
+let name;
+
+name = "John";
+admin = name;
+console.log(admin);
+
+// Giving the right name
+const EARTH = "Earth";
+let currentUser = "John";
+
+//Uppercase const?
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+
+
