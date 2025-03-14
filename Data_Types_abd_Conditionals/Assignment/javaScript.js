a903af8d595450c7471e2caaf00f5faa7d09336f
Maria <100872877+Maria00002@users.noreply.github.com>
2025-03-14 12:12:03 -0700

    JavaScript.info's lesson on if/else
    Code and finish the 'tasks' at the bottom of the
    page. Learn and review the concpet of conditional
    branching: if,'?'

--- Data_Types_abd_Conditionals/Assignment/javaScript.js
+++ Data_Types_abd_Conditionals/Assignment/javaScript.js
@@ -47,5 +47,42 @@ else {
     console.log("I don't know you");
 }
 
+////////////////////////////////////////////////////////////////
+////Below is the practice from JavaScript.info's lesson on if/else
+
+// Using the if...else construct, write the code which asks: 
+// "WHat is the "official name" name of JavaScript?"
+
+let message = prompt('What is the "official" name of JavaScript', '');
+message=="ECMAScript"? alert("Right!"):alert("You don't know? ECMAScript!");
+
+// Show the sign
+// Using if...else, write the code which gets a number via prompt and then shows in alert
+// 1, if the value is greater than zero
+// -1, if less than zero
+// 0, if equals zero
+let answer = prompt("Please enter a number",'');
+if (answer > 0){
+    alert("1");
+}
+else if (answer < 0){
+    alert("-1");
+}
+else if (answer == 0){
+    alert("0");
+}
+
+// Rewrite 'if' into '?'
+// Rewrite this if using the condiotnal opeator '?'
+let result;
+result=(a+b<4)?'Below':'Over';
+
+//// Rewrite 'if...else' into '?'
+//// Rewrite if...else using multiple ternary operators '?'
+let message;
+message = (login == 'Employee')? 'Hello':
+(login == 'Director')? 'Greetings':
+(login == '')? 'No login':
+'';
 
 
