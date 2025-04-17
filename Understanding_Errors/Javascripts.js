e6b4684f671a5080de8c2e67bdba5e3dca7d779c
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-17 10:29:27 -0700

    Understanind Errors
    Code and summarized the following topics:
    Introduction, Lesson overview, the anatomy of an
    error, Common types of errors (syntax error,
    reference error, type error), tips for resolving
    errors

---
+++ Understanding_Errors/Javascripts.js
@@ -0,0 +1,66 @@
+// Lesson Overview
+// Name at least three kinds of JavaScript errors
+// Identify two parts of an error message that help you find where the error originates
+// Be able to understand how to research and resolve errors
+
+// ReferenceError: a variable that is not declared and/or initialized within
+// the current scope. 
+// const a = "Hello";
+// const b = "World";
+
+// console.log(c);
+
+//Stack Trace
+// const a = 5;
+// const b = 10;
+
+// function add(){
+//     return c;
+// }
+
+// function print(){
+//     add();
+// }
+
+// print();
+
+// Common Types of errors
+// Syntax error
+// function helloWorld(){
+//     console.log "Hello World!"
+// }
+
+
+// Reference Error
+// Type Error
+// An operand or argument passsed to a funciton is incompatible with the type
+// expected by that operator or function
+// or when ttempting to modify a value that cannot be changed
+// or when attemping to use a value in an inappropriate way
+
+// const str1 = "Hello";
+// const str2 = "World!";
+// const message = str1.push(str2);
+
+
+// Tips for resolving errors
+// 1 try understand the error MessageChannel. It shows which lines to examine 
+// to find the source of the error
+// 2 Google the error
+// 3 Use the debugger. set breakpoints. view the value of any given variable 
+// at any PointerEvent, step through code line by line
+// 4 Make use of the console.  
+
+// Errors vs. warnings
+// Errors will stop the 
+//
+//
+
+
+
+
+
+
+
+
+
