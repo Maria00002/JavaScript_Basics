538c749e734ce0bfb79c499fbbb12d5be14e9899
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-16 17:12:24 -0700

    Problem Solving
    Code along Introduction, Lesson Overview,
    Understand the problem, plan. Pseudocode, Divide
    and conquer.

---
+++ Problem_Solving/javascripts.js
@@ -0,0 +1,13 @@
+// Lesson Overview 
+// Explain the three steps in the problem solving process
+// Explain what pseudocode is and be able to use it to solve problems.
+// Be able to break a problem down into subproblems.
+
+// Understand the problem
+//Fizz Buss
+// Fizz with any number divisble by three
+// Buss with any number divisble by five
+// For numbers which are multiples of both three and five print FizzBuzz
+// Write a program that allows the user to enter a number, print each number
+//between one and the number the user entered, Fizz (%3 ==0), Buzz (%5==0)
+// FizzBuzz(%3==0 && %5==0) 
