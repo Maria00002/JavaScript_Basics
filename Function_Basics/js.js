81cdf5513364ab00ef6bfe7e4a00ab9bee5d48bd
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-20 14:59:43 -0700

    Revisit Functions - reusable blocks of code
    Code along with the examples

---
+++ Function_Basics/js.js
@@ -0,0 +1,17 @@
+function favouriteAnimal(animal){
+    return animal + " is my favourite animal!";
+}
+
+console.log(favouriteAnimal('goat'));
+
+// mdn web docs
+// Functions-reusable blocks of code
+const myText = "I am a string";
+const newString = myText.replace("string","sausage");
+console.log(newString);
+
+const myArray = ["I","love","chocolate","frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+
+const myNumber = Math.random();
