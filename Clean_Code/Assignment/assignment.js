b2d95708138868332d045484d80fcd09c8e398ac
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-21 19:41:50 -0700

    Summarize the reading
    Read the article "10 Principles for Keeping Your
    Programming Code Clean" by Pamela and Summarize
    them into shorter readable notes.

---
+++ Clean_Code/Assignment/assignment.js
@@ -0,0 +1,33 @@
+// 10 Principles for Keeping Your Programming Code Clean
+// 1. Revise Your Logic Before Coding
+// Can have some flow diagrams or written pseudo-code to verify the logic 
+// Writing it down can clarify many doubts or insecuitities about complex funcionality.
+
+// 2. Clearly Expose the Structure of the Page
+
+// 3. Use the Correct Indentation
+
+// 4. Write Explanatory Comments
+
+// 5. Avoid Abusing Comments 
+// The current funciotnality is explained in terms of variables and results.
+// Exanmples of good comment use are:
+// Authoring specifications 
+// Detailed statements on the funcationality of a mthod of procedure
+// Quick notifications or labels that state where a recent change was made
+
+// 6. Avoid Extremely Large Functions
+// Break up large functions into smaller ones. 
+
+// 7. Use Naming Standards for FUnctions and Variables
+// Whemever a variable or a funciton is created, its name should be descriptive
+// enough as to give a general idea of what it does or what it is for. 
+
+// 8. Treat Changes with Caution
+
+// 9. Avoid Indiscriminate Mixing of Coding Languages  
+
+// 10. Summarize Your Imports 
+// If there are too many style sheets, they can probably be 
+// summarized into one or two. 
+// THis will save space and make things look cleaner, and also save loading time
