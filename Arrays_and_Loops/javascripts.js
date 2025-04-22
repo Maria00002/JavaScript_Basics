8098264afb92f4d3b1dab4447bdf2f0a6024a1a8
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-22 13:39:52 -0700

    JavaScript Arrays
    Code along the examples

---
+++ Arrays_and_Loops/javascripts.js
@@ -0,0 +1,11 @@
+// arrays, loops, Test-Driven Development(TDD)
+// Array: an ordered collection of items (strings, numbers, or other things)
+// JavaScript Arrays from W3Schools
+const cars = ["Saab","Volvo","BMW"];
+console.log(cars);
+
+const carsA = [];
+carsA[0] = "Saab";
+carsA[1] = "Volvo";
+carsA[2] = "BMW";
+console.log(carsA);
