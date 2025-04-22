ed32bbea0eec3c981fbdba1c09b73ebffed29606
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-22 14:25:43 -0700

    w3schools JS Arrays
    Code along the examples

--- Arrays_and_Loops/javascripts.js
+++ Arrays_and_Loops/javascripts.js
@@ -9,3 +9,24 @@ carsA[0] = "Saab";
 carsA[1] = "Volvo";
 carsA[2] = "BMW";
 console.log(carsA);
+
+let car = cars[0];
+console.log(car); //Saab
+
+cars[0] = "Opel";
+console.log(cars);
+
+const fruits = ["Banana","Orange","Apple","Mango"];
+//toString() converts an array to a string of (comma separated) array values
+let fruitString = fruits.toString();
+console.log(fruitString);
+
+console.log(cars);
+
+// Arrays are special type of objects. 
+// The typeof operator returns "object" for arrays
+const person = ["John","Doe",46];
+console.log(person[0]); 
+
+const person1 = {firstName:"John", lastName:"Doe", age:46};
+console.log(person1.lastName);
