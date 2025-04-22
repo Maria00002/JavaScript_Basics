c06e5a0c0573c635c7479b6113fbccbd26a09189
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-22 14:52:31 -0700

    JS Arrays
    Continue code along the examples provided.

--- Arrays_and_Loops/javascripts.js
+++ Arrays_and_Loops/javascripts.js
@@ -30,3 +30,65 @@ console.log(person[0]);
 
 const person1 = {firstName:"John", lastName:"Doe", age:46};
 console.log(person1.lastName);
+
+// Array ELements Can Be Objects
+// JavaScipt varaibles can be objects. Arrays are special kinds of objects.
+// myArray[0] = Date.now;
+// myArray[1] = myFunction;
+// myArray[2] = myCars;
+
+// Array Properties and Methods
+cars.length;
+cars.sort();
+
+console.log(cars.sort());
+console.log(cars);
+
+const fruitsA = ["Banana","Orange","Apple","Mango"];
+let length = fruits.length;
+console.log(length);
+
+// Accessing the First Array Element
+let fruit = fruitsA[0];
+console.log(fruit); //Banana
+
+// Accessing the Last Array Element
+let fruitLast = fruitsA[fruitsA.length - 1];
+console.log(fruitLast);
+
+{/* <p id="demo"></p> */}
+//Looping Array Elements
+let fLen = fruits.length;
+
+// let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+//     text += "<li>" + fruits[i] + "</li>";
+// }
+
+// text += "</ul>"
+// document.getElementById("demo").innterHTML = text;
+console.log("Using a for loop: ");
+for (let i = 0; i < fLen; i++) {
+    let print = fruits[i];
+    console.log(print);
+}
+console.log("Array.forEach(): ");
+// Can also use Array.forEach() function 
+fruits.forEach(myFunction);
+
+function myFunction(currentValue) {
+    console.log(currentValue);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
