b3456ca0d9f0f080546f4043bff56cf7008fc6ab
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-25 11:41:03 -0700

    DOM Maniupulation and Events

---
+++ DOM_Manipulation_and_Events/javascripts.js
@@ -0,0 +1,6 @@
+// Explain what the DOM is in relation to a webpage
+// Explain the difference between a "node" and an "element"
+// Explain how to target nodes with "selectors"
+// Explain the basic methods for finding, adding, removing, and altering DOM Nodes
+// Explain what "bubbling" is and how it works
+
