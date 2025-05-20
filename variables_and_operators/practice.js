5ac5f729dbde4e78242345a47f726c6609a0e753
Maria <100872877+Maria00002@users.noreply.github.com>
2025-05-20 14:35:49 -0700

    Code along the examples provided
    On Variables and Operators

---
+++ variables_and_operators/practice.js
@@ -0,0 +1,35 @@
+let name = "John";
+let surname = "Doe";
+let age = 11;
+
+console.log(name);
+console.log(surname);
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14
+pi = 10;
+console.log(pi);
+
+console.log((3+2)-76*(1+1));
+
+//// Assignment
+console.log(23+97);
+console.log(1+2+3+4+5+623+97);
+console.log((4+6+9)/77);
+
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let b = 7*a;
+console.log(b); //140
+
+const max = 57;
+const actual = max-13;
+const percentage = actual/max;
+console.log(percentage);
