6da01d02331f13fd996309dd6f431462f8870bc4
Maria <100872877+Maria00002@users.noreply.github.com>
2025-05-21 17:56:40 -0700

    Data Types
    Follow and code along the section "JavaScript
    Fundamentals" on Data Types

---
+++ Data_Types_abd_Conditionals/javascript.js
@@ -0,0 +1,24 @@
+//////////// The JavaScript language - JavaScript Fundamentals
+// Data Types
+// console.log(1/0);
+// console.log(Infinity);
+// console.log("not a number"/2);
+
+// let name = "John";
+// console.log(`Hello, ${name}`);
+
+// let age = null;
+// let age2;
+// console.log(age2);
+
+let name = "Ilya";
+console.log(`hello ${1}`); // hello 1
+console.log(`hello ${"name"}`); // hello name
+console.log(`hello ${name}`); //hello Ilya
+
+
+
+
+
+
+
