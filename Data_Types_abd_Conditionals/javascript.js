9052e51f6a9faf54e61867b0b4d4b03fb24664dc
Maria <100872877+Maria00002@users.noreply.github.com>
2025-05-21 18:51:48 -0700

    Handling text - strings in javascript
    Follow and code along of the sections "Handling
    text - strings in JavaScript" from mdn web docs

--- Data_Types_abd_Conditionals/javascript.js
+++ Data_Types_abd_Conditionals/javascript.js
@@ -11,10 +11,58 @@
 // let age2;
 // console.log(age2);
 
-let name = "Ilya";
-console.log(`hello ${1}`); // hello 1
-console.log(`hello ${"name"}`); // hello name
-console.log(`hello ${name}`); //hello Ilya
+// let name = "Ilya";
+// console.log(`hello ${1}`); // hello 1
+// console.log(`hello ${"name"}`); // hello name
+// console.log(`hello ${name}`); //hello Ilya
+
+//////////////// Handling text - strings in JavaScript
+// const string = "The revolution will not be televised.";
+// console.log(string);
+
+// const badString = string;
+// console.log(badString);
+
+// const name = "Chris";
+// const greeting = `Hello, ${name}`;
+// console.log(greeting);
+
+// const one = "Hello, ";
+// const two = "how are you?";
+// const joined = `${one}${two}`;
+// console.log(joined);
+
+// const greeting = "Hello";
+// const name = "Chris";
+// console.log(greeting + ", " + name); 
+// console.log(`${greeting}, ${name}`);
+
+
+// const newline = `One day you finally knew
+// what you had to do, and began,`;
+// console.log(newline);
+
+// const bigmouth = 'I\'ve got no right to take my place...'; //escaping symbol /
+// console.log(bigmouth);
+
+// const name = "Front ";
+// const number = 242;
+// console.log(name + number);
+
+const myString = "123";
+const myNum = Number(myString);
+console.log(typeof myNum);
+
+const myNum2 = 123;
+const myString2 = String(myNum2);
+console.log(typeof myString2);
+
+
+
+
+
+
+
 
 
 
