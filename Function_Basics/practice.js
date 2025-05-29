ebbce387854d4af96723c35aa373dc49c0604c06
Maria <100872877+Maria00002@users.noreply.github.com>
2025-05-29 13:20:50 -0700

    Functions
    Follow and code along the examples from "Functions"
    on Javascript.Info

--- Function_Basics/practice.js
+++ Function_Basics/practice.js
@@ -41,24 +41,185 @@
 // console.log(newString);
 
 
-function squared(num) {
-    return num * num;
-}
+// function squared(num) {
+//     return num * num;
+// }
 
-function cubed(num) {
-    return num * num * num;
-}
+// function cubed(num) {
+//     return num * num * num;
+// }
 
-function factorial(num) {
-    if (num < 0 ) return undefined;
-    if (num === 0) return 1;
-    let x = num - 1;
-    while (x > 1) {
-        num *= x;
-        x--;
+// function factorial(num) {
+//     if (num < 0 ) return undefined;
+//     if (num === 0) return 1;
+//     let x = num - 1;
+//     while (x > 1) {
+//         num *= x;
+//         x--;
+//     }
+//     return num;
+// }
+
+//////////// Functions
+// function showMessage() {
+//     alert('Hello everyone!');
+// }
+
+// showMessage();
+// showMessage();
+
+// function showMessage() {
+//     let message = "Hello, I'm JavaScript!";
+//     alert(message);
+// }
+
+// showMessage();
+// alert(message);
+
+// let userName = "John";
+
+// function showMessage() {
+//     userName = "Bob";
+//     let message = "Hello, " + userName;
+//     alert(message); 
+// }
+// alert(userName);
+// showMessage();
+// alert(userName);
+
+// let userName = "John";
+
+// function showMessage() {
+//     let userName = "Bob"; // declare a local variable
+//     let message = "Hello, " + userName;
+//     alert(message); 
+// }
+// alert(userName);
+// showMessage();
+// alert(userName);
+
+
+// function showMessage(from, text) {
+//     alert(from + ": " + text);
+// }
+
+// showMessage("Ann","Hello!");
+// showMessage("Ann","What's up?");
+
+
+// function showMessage(from, text) {
+//     from = "*" + from + "*";
+//     alert(from + ": " + text);
+// }
+// // let from = "Ann";
+// // showMessage(from,"Hello!");
+// // alert(from);
+
+// showMessage("Ann");
+
+// function showMessage(from, text = "no text given") {
+//     alert(from + ": " + text);
+// }
+
+// showMessage("Ann", undefined);
+
+
+// function showMessage(from, text) {
+//     if (text === undefined) {
+//         text = "no text given";
+//     }
+//     alert(from + ": " + text);
+// }
+
+// showMessage("Ann");
+
+
+// function showMessage(from, text) {
+//     text = text || "no text given";
+//     alert(from + ": " + text);
+// }
+
+// showMessage("Ann");
+
+// function showMessage(text) {
+//     if (text === undefined) {
+//         text = "empty message";
+//     }
+
+//     alert(text);
+// }
+
+// showMessage();
+
+// function showMessage(text) {
+//     text = text || "empty";
+// }
+
+
+// function showCount(count) {
+//     alert(count ?? "unknown");
+// }
+
+// showCount(0);
+// showCount(null);
+// showCount();
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// let result = sum(1,2);
+// alert(result);  
+
+// function checkAge(age) {
+//     if (age >= 18) {
+//         return true;
+//     } else {
+//         return confirm("Do you have permission from your parents?");
+//     }
+// }
+
+// let age = prompt("How old are you?",18);
+
+// if(checkAge(age)){
+//     alert("Access granted");
+// } else {
+//     alert("Access denied");
+// }
+
+// function doNothing() {return;}
+// alert(doNothing() === undefined);
+
+// function showPrimes(n) {
+//     nextPrime: for (let i = 2; i < n; i++) {
+//         for (let j = 2; j < i;j++) {
+//             if (i% j == 0) continue nextPrime;
+//         }
+
+//         alert(i); 
+//     }
+// }
+
+// showPrimes(10);
+
+function showPrimes(n) {
+    for (let i = 2; i < n; i++) {
+        if (!isPrime(i)) continue;
+        alert(i);
     }
-    return num;
 }
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+
+
+
+
 
 
 
