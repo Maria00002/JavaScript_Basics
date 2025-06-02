01079e1892f6729605a0119117f0c89ef54e5796
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-02 11:03:43 -0700

    Understanding Errors
    Code and follow along with the sections provided
    by The Odin Project

---
+++ Understanding_Errors/practice.js
@@ -0,0 +1,43 @@
+//// Syntax error
+// function helloWorld() {
+//     console.log "Hello World!"
+// }
+
+////// Reference error
+//////// Type error
+////// An operand or argument passed to a function is inconpatible with
+///// the type expected by that operator or function
+///// or when attempting to modify a value that cannot be changed
+///// or when attempting to use a value in an inappropriate way
+const str1 = "Hello";
+const str2 = "World!";
+const message = str1.push(str2);
+
+
+
+
+
+// const a = 5;
+// const b = 10;
+
+// function add() {
+//     return c;
+// }
+
+// function print() {
+//     add();
+// }
+
+// print();
+
+
+
+
+
+
+// const a = "Hello";
+// const b = "World";
+
+// console.log(c);  // ReferenceError; A ReferenceError is thrown when one 
+// refers to a varaiable that is not declared and/or initialized within
+// the current scope.
