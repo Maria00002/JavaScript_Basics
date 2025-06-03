757ed17f630cced987ba1c497aba043bfcdbc862
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-03 11:09:49 -0700

    10 Principles for Keeping Your Programming Code Clean

--- Clean_Code/practice.js
+++ Clean_Code/practice.js
@@ -1,16 +1,37 @@
-const x = function (z) {
-    const w = "Hello ";
-return w + z;
-};
+/////// Clean Code
 
-x("John");
-console.log(x("John"));
+// const x = function (z) {
+//     const w = "Hello ";
+// return w + z;
+// };
+
+// x("John");
+// console.log(x("John"));
 
 
-const generateUserGreeting = function (name) {
-    const greeting = "Hello ";
-    return greeting + name;
-};
+// const generateUserGreeting = function (name) {
+//     const greeting = "Hello ";
+//     return greeting + name;
+// };
 
-generateUserGreeting("John");
-console.log(generateUserGreeting("John"));
+// generateUserGreeting("John");
+// console.log(generateUserGreeting("John"));
+
+//////// 10 Principles for Keeping Your Programming Code Clean 
+///// 1 Revise Your Logic Before Coding
+///// 2 Clearly Expose the Structure of the Page
+///// 3 Use the Correct Indentation
+///// 4 Write Explanatory Comments
+///// 5 Avoid Abusing Comments
+///// 6 Avoid Extremely Large Functions 
+///// 7 Use Naming Standards for Functions and Variables
+///// 8 Treat Changes with Caution
+///// 9 Avoid Indiscriminate Mixing of Coding Languages
+///// 10 Summarize Your Imports
+
+
+
+
+
+
+
