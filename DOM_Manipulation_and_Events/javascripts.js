5f6a3d2406f23a564d77c98ee2a5aba29d0ae8eb
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-24 17:45:26 -0700

    DOM Manipulation and Events
    Go over the following sections: Introduction,
    Lesson overview, Document Object Model, Targeting
    nodes with selectos, DOM methods.

--- DOM_Manipulation_and_Events/javascripts.js
+++ DOM_Manipulation_and_Events/javascripts.js
@@ -123,21 +123,134 @@
 
 
 // My JavaScript file
-const container = document.querySelector("#container");
+// const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+// const content = document.createElement("div");
+// content.classList.add("content");
+// content.textContent = "This is the glorious text-content!";
 
-container.appendChild(content);
+// container.appendChild(content);
+
+// What the DOM is in relation to a webpage
+// the difference bt a "node" and an "element"
+// how to target nodes with "selectors"
+// the basic methods for finding, adding, removing, and altering DOM nodes
+// the difference bt a "NodeList" and an "array of nodes"
+// what "bubbling" is and how it works
+
+// Document Object Model
+/* <div id="container">
+    <div class="display"></div>
+    <div class="controls"></div>
+</div> */
+
+// Targeting nodes with selectos
+// CSS-style selectos for <div class="display"></div>
+// div.display
+// .display
+// #container > .display
+// div#container > div.display
+
+// selects the #container div 
+// const container = document.querySelector("#container");
+
+// // selects the first child of #container => .display
+// const display = container.firstElementChild;
+// console.log(display);
+
+// selects the .controls div
+// const controls = document.querySelector(".controls");
+// // selects the prior sibling => .display
+// const display =controls.previousElementSibling;
+// console.log(display); // <div class="display"></div>
+
+// DOM methods
+// WHen the HTML code is parsed by a web browser, it is converted to the DOM
+// these nodes are JavaScript objects that have many properties and methods
+// attached to them. 
+// Query selectos
+// element.querySelector(selector) // returns a reference to the first match of selector
+// element.querySelectorAll(selectors) // returns a "NodeList" containing references
+                                    // to all of the matches of the selectors
 
 
+// Element creation
+// document.createElement(tagName,[options]) // creates a new element
+// of tag type tagName. [options] means you can add some optional parameters
+// to the function
 
+// This function does not put the new element into the DOM
+// You can place the element into the DOM with one of the following methods
+// Append elements
+// parentNode.appendChild(childNode) // appends childNode as the last child 
+//                                   // of parentNode
+// parentNode.insertBefore(newNode, referenceNode) 
+// // inserts newNode into parentNode before referenceNode
 
+// // Remove elements
+// parentNode.removeChild(child) // removes child from parentNode on the DOM
+// and returns a reference to child
 
+// Altering elements
+// When you have a reference to an element, you can use that reference to
+// alter the element's own properties
 
+// creates a new div referenced in the variable 'div'
+// const div = document.createElement("div");
 
+// adds the indicated style rule to the element in the div variable
+// div.style.color = "blue";
 
+// // adds several style rules
+// div.style.cssText = "color: blue; background: white;";
+
+// // adds several style rules
+// div.setAttribute("style","color: blue;background: white;");
+
+// dot notation with kebab case: doesn't work as it attempts to subtract
+// color from div.style.background
+// equivalent to : div.style.background - color
+// div.style.background-color;
+
+// //dot notation with camelCase: works, accesses the div's background-color style
+// div.style.backgroundColor;
+
+// // bracket notation with kebab-case: also works
+// div.style["background-color"];
+
+// // bracket notation with camelCase: also works
+// div.style["backgroundColor"];
+
+// Editing attributes
+// if id exists, update it to 'theDiv', else create an id with value "theDiv"
+// div.setAttribute("id","theDiv");
+
+// // returns value of specified attribute, in this case "theDiv"
+// div.getAttribute("id");
+
+// // removes specified attribute
+// div.removeAttribute("id");
+
+// mdn web docs
+// HTML attribute reference
+
+// Working with classes
+// adds class "new" to your new div
+// div.classList.add("new");
+
+// // removes "new" class from div
+// div.classList.remove("new");
+
+// // if div doesn't have class "active" then add it, or if it does, then remove it
+// div.classList.toggle("active");
+
+// Adding text content
+// creates a text node containing "Hello World!" and inserts it in div
+// div.textContent = "Hello World!";
+
+// Adding HTML content
+// renders the HTML inside div
+// div.innerHTML = "<span>Hello World!</span>";
 
 
 
