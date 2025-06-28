5e98ed89f1ef9cd747012806dd3c1a71bc6fa295
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-27 19:40:04 -0700

    Exercise
    DOM Manipulation and Events

--- DOM_Manipulation_and_Events/javascripts.js
+++ DOM_Manipulation_and_Events/javascripts.js
@@ -435,35 +435,85 @@ button.addEventListener("click",myFunction);
  */
 
 // Testing parentNode.insertBefore
+// const container = document.querySelector("#container");
+
+// const content = document.createElement("div");
+// content.textContent = "This is some text";
+// container.insertBefore(content, two); 
+
+
+// // console.log(container.removeChild(content)); // return "this is some text"
+
+// // const div = document.createElement("div");
+
+// content.style.color = "blue";
+
+// content.style.cssText = "color: blue; background: white;";
+
+// content.setAttribute("style","color: blue; background: yellow;");
+
+// // Need to use camelCase with dot notation or bracket notation
+
+// content.style.backgroundColor = "Pink";
+// content.style["background-color"] = "green";
+// content.style["backgroundColor"] = "white";
+
+// content.setAttribute("id","theDiv");
+
+// let result = content.getAttribute("id"); //theDiv
+// // console.log(result);
+
+// content.removeAttribute("id");
+
+// // Working with classes
+// content.classList.add("new");
+
+// content.classList.remove("new");
+
+// // if content doesn't have class "active" then add it, or if it does,
+// // then remove it
+// content.classList.toggle("active");
+
+
+// const container = document.querySelector("#container");
+// const content = document.createElement("div");
+// content.classList.add("content");
+// content.textContent = "This is the glorious text-content!";
+
+// container.appendChild(content);
+
+// Exercise
+// 1 a <p> with red text that says "Hey I'm red!"
 const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.textContent = "This is some text";
-container.insertBefore(content, two); 
+const p = document.createElement("p");
+p.style.color = "red";
+p.textContent = "Hey I'm red!";
+container.appendChild(p);
 
+// 2 an <h3> with blue text that says "I'm a blue h3!"
+const h3 = document.createElement("h3");
+h3.style.color = "blue";
+h3.textContent = "I'm a blue h3!";
+container.appendChild(h3);
 
-// console.log(container.removeChild(content)); // return "this is some text"
+// 3 a <div> with a black border and pink background color with the 
+// following elements inside of it 
+const div = document.createElement("div");
+div.style.borderStyle = "solid";
+div.style.backgroundColor = "pink";
+// another <h1> that says "I'm in a div"
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
 
-// const div = document.createElement("div");
+// a <p> that says "ME TOO!"
+const p2 = document.createElement("p");
+p2.textContent = "ME TOO!";
 
-content.style.color = "blue";
+div.appendChild(h1);
+div.appendChild(p2);
+container.appendChild(div);
 
-content.style.cssText = "color: blue; background: white;";
-
-content.setAttribute("style","color: blue; background: yellow;");
-
-// Need to use camelCase with dot notation or bracket notation
-
-content.style.backgroundColor = "Pink";
-content.style["background-color"] = "green";
-content.style["backgroundColor"] = "white";
-
-content.setAttribute("id","theDiv");
-
-let result = content.getAttribute("id"); //theDiv
-// console.log(result);
-
-content.removeAttribute("id");
 
 
 
