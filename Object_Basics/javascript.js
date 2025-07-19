fe1a5e5e29d9f9e6ea4f53c24b11aee6692f17c5
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-19 13:53:09 -0700

    Practice different array methods
    reduce

--- Object_Basics/javascript.js
+++ Object_Basics/javascript.js
@@ -705,7 +705,19 @@ usersById = {
 
 // console.log(numbers);
 
+// const numbers = [175, 50, 25];
+// console.log(numbers.reduce(myFunc));
 
+// function myFunc(total, num) {
+//     return total - num;
+// }
+
+const numbers = [15.5,2.3,1.1,4.7];
+console.log(numbers.reduce(getSum,0));
+
+function getSum(total, num) {
+    return total + Math.round(num);
+}
 
 
 
