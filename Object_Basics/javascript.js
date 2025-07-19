7afd2a33be059b8313cd8a2df0b257ea356cebf5
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-18 18:49:44 -0700

    Array methods

--- Object_Basics/javascript.js
+++ Object_Basics/javascript.js
@@ -365,6 +365,275 @@
 // const myNotification = new Notification("Hello!");
 
 // Test you skills: Object basics
+// function increaseCounterObject(objectCounter) {
+//     objectCounter.counter += 1;
+// }
+
+// function increaseCounterPrimitive(primitiveCounter) {
+//     primitiveCounter += 1;
+// }
+
+// let object = {counter: 0};
+// let primitive = 0;
+
+// console.log(object);
+// increaseCounterObject(object)
+// console.log(object);
+
+// console.log(primitive);
+// increaseCounterPrimitive(primitive);
+// console.log(primitive);
+
+// let animal = {species: "dog"};
+// let dog = animal;
+// let exp = dog;
+
+// animal = {species: "cat"};
+// dog = {species: "somethingElse"};
+
+// console.log(animal);
+// console.log(dog);
+// console.log(exp);
+
+// Assignment
+// Map to names
+
+// let john = {name: "John", age: 25};
+// let pete = {name: "Pete", age: 30};
+// let mary = {name: "Mary", age: 28};
+
+// let users = [john, pete, mary];
+// // console.log(users);
+// let names = users.map(item => item.name)
+// console.log(names);
+
+
+// Map to objects
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = users.map(user => ({
+//     fullName: `${user.name} ${user.surname}`,
+//     id: user.id    
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+
+
+// Sort users by age
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// sortByAge(arr);
+
+// function sortByAge(arr) {
+//     arr.sort((a,b) => a.age-b.age);
+// }
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+
+// Get average age
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// console.log(getAverageAge(arr));
+
+// function getAverageAge(arr){
+//     let result = arr.reduce((prev, user) => prev + user.age, 0);
+//     result = result/arr.length;
+//     return result;
+
+// }
+
+
+// Create keyed object from array
+// let users = [
+//   {id: 'john', name: "John Smith", age: 20},
+//   {id: 'ann', name: "Ann Smith", age: 24},
+//   {id: 'pete', name: "Pete Peterson", age: 31},
+// ];
+
+// let usersById = groupById(users);
+
+// function groupById(array) {
+//     return array.reduce((obj, value) => {
+//         obj[value.id] = value;
+//         return obj;
+//     }, {})
+// }
+
+// console.log(usersById);
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+
+// let arr = ["I","go","home"];
+// delete arr[1];
+// console.log(arr[1]);
+// console.log(arr);
+
+// let arr = ["I","study","javascript"];
+// arr.splice(1,1);
+// console.log(arr);
+
+// arr.splice(0,3,"Let's","dance");
+// console.log(arr);
+
+// let removed = arr.splice(0,2)
+// console.log(removed);
+// console.log(arr);
+
+// arr.splice(2,0,"complex","language");
+// console.log(arr);
+
+// let arr = [1,2,5];
+// arr.splice(-1,0,3,4);
+// console.log(arr);
+
+// let arr = ["t","e","s","t"];
+// console.log(arr.slice(1,3)); // es
+// console.log(arr.slice(-2)); // st
+
+// let arr = [1,2];
+
+// console.log(arr.concat([3,4]));
+// console.log(arr.concat([3,4],[5,6]));
+// console.log(arr.concat([3,4], 5,6));
+
+// let arrayLike = {
+//     0: "something",
+//     1: "else",
+//     [Symbol.isConcatSpreadable]: true,
+//     length: 2
+// };
+
+// console.log(arr.concat(arrayLike));
+
+// arr.forEach({
+
+// }) ;
+
+// ["a","b","c"].forEach(alert);
+
+// ["a","b","c"].forEach((item,index,array) => {
+//     console.log(`${item} is at ${index} in ${array}`);
+// });
+
+// let arr = [1,0, false];
+// console.log(arr.indexOf(0));
+// console.log(arr.indexOf(false));
+// console.log(arr.indexOf(null));
+// console.log(arr.includes(false));
+
+// let fruits = ["Apple","Orange","Apple"]
+// console.log(fruits.indexOf("Apple"));
+// console.log(fruits.lastIndexOf("Apple"));
+
+// let users = [
+//     {id: 1, name: "John"},
+//     {id: 2, name: "Pete"},
+//     {id: 3, name: "Mary"},
+//     // {id: 4, name: "John"}
+// ];
+
+// // let user = users.find(item => item.id == 1);
+// // console.log(user.name);
+
+// // console.log(users.findIndex(user => user.name == "John"));
+// // console.log(users.findLastIndex(user => user.name == "John"));
+
+// let someUsers = users.filter(item => item.id < 3);
+// console.log(someUsers);
+
+// let arr = ["Bilbo","Gandalf","Nazgul"];
+// let result = arr.map(item => item.length);
+// console.log(result);
+// console.log(arr);
+
+// let arr = [1,2,15];
+// arr.sort();
+// console.log(arr);
+
+// function compare(a,b) {
+//     if (a > b) return 1;
+//     if (a == b) return 0;
+//     if (a < b) return -1;
+// }
+
+// function compareNumeric(a,b) {
+//     return a - b;
+// }
+
+// let arrar = [1,2,15];
+// arrar.sort(compareNumeric);
+// console.log(arrar);
+
+// let arr = [1,2,3,4,5];
+// arr.reverse();
+// console.log(arr);
+
+// let names = "Bilbo, Gandalf, Nazgul";
+// let arr = names.split(", ", 2);
+// for (let name of arr) {
+//     console.log(`A message to ${name}.`);
+// }
+
+// let str = "test";
+// console.log(str.split(""));
+
+// let arr = ["Bilbo", "Gandalf", "Nazgul"];
+// let str = arr.join("; ");
+// console.log(str);
+
+// Reduce. reduceRight
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log();
+
+
+
 
 
 
